37aeae0e55ad377b5019a2b687a6875a1f9d04d8
Lakshmanhoney18 <93142744+Lakshmanhoney18@users.noreply.github.com>
2021-11-03 18:41:28 +0530

    Add files via upload

---
+++ arthematic operations.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+
+{
+	int a=23 ,b=4;
+	int addition , subtraction, multiplication, division, modulus;
+	addition = a+b;
+	subtraction = a-b;
+	multiplication = a*b;
+	division=a/b;
+	modulus=a%b;
+	
+	printf ("addition of two numbers a,b is : %d\n",addition );
+    printf ("subtrction  of two numbers a,b is : %d\n",subtraction );
+	printf (" multiplication of two numbers a,b is : %d\n",multiplication  );
+	printf ("division of two numbers a,b is : %d\n",division  );
+    printf ("modulus of two numbers a,b is : %d\n",modulus );
+}
---
+++ marks.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int total;
+    int physics=60;
+    int chemistry=70;
+    int maths=65;
+    int english=90;
+	printf("marks in physics =%d\n",physics);
+	printf("marks in chemistry =%d\n",chemistry);
+	printf("marks in maths =%d\n",maths);
+	printf("marks in english =%d\n",english);
+	total= physics+chemistry+maths+english;
+	printf("total physics and chemistry and maths and english marks =%d",total);
+}
