61a103802650f351069010cb50b0bc1c07a207bd
Lakshmanhoney18 <93142744+Lakshmanhoney18@users.noreply.github.com>
2021-11-08 11:49:16 +0530

    Add files via upload

---
+++ a operation.exercise.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c,d;
+	a=20;
+	b=10;
+	
+	printf("%d\n",a>b );
+	printf("%d\n",a>=b  );
+	printf("%d\n",a<b );
+	printf("%d\n",a<=b );
+	printf("%d\n",a==b );
+	printf("%d\n",a!=b );
+	
+}
